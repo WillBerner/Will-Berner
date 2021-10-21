1ba2b3464b117ec6dec082022f3780fc895e047f
Will Berner <will.o.berner@gmail.com>
2021-10-21 02:03:55 -0400

    Added bold font to currently viewed page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,23 +10,23 @@ export default function Header({ setPage, currentPage }) {
     return (
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <div className="container-fluid">
-            <button className="navbar-brand btn mt-1 fw-bold" onClick={setAbout} >Will Berner</button>
+                <button className="navbar-brand btn mt-1 fw-bold" onClick={setAbout} >Will Berner</button>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <button className="btn mt-1" onClick={setAbout} >About Me</button>
+                            <button className={currentPage === "About" ? "btn mt-1 fw-bold" : "btn mt-1"} onClick={setAbout} id="about">About Me</button>
                         </li>
                         <li className="nav-item">
-                            <button className="btn mt-1" onClick={setPortfolio}>Portfolio</button>
+                            <button className={currentPage === "Portfolio" ? "btn mt-1 fw-bold" : "btn mt-1"} onClick={setPortfolio} id="portfolio">Portfolio</button>
                         </li>
                         <li className="nav-item">
-                            <button className="btn mt-1" onClick={setContact}>Contact</button>
+                            <button className={currentPage === "Contact" ? "btn mt-1 fw-bold" : "btn mt-1"} onClick={setContact} id="contact">Contact</button>
                         </li>
                         <li className="nav-item">
-                            <button className="btn mt-1" onClick={setResume}>Resume</button>
+                            <button className={currentPage === "Resume" ? "btn mt-1 fw-bold" : "btn mt-1"} onClick={setResume} id="resume">Resume</button>
                         </li>
                     </ul>
                 </div>
